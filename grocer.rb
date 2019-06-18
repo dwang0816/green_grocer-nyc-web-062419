1fa11e24d99fa3e44bd6e453cd591875fc1116e1
dennis wang <dwang0816@gmail.com>
2019-06-18 18:52:25 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -1,6 +1,21 @@
 def consolidate_cart(cart)
   # code here
-  
+  item_list = []
+  consolidated = cart.uniq
+  consolidated.each {|hash| hash.values[0][:count] = 0 }
+  cart.each { |hash| item_list << hash.keys }
+  item_list.flatten.each do |item|
+    consolidated.each do |hash|
+      if hash.keys[0] == item
+        hash.values[0][:count] += 1
+      end
+    end
+  end
+    return_hash = {}
+    consolidated.each do |hash|
+      return_hash[hash.keys.first] = hash.values.first
+    end
+    return_hash
 end
 
 def apply_coupons(cart, coupons)
